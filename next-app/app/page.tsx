b3b5bb8ae68651d86e0a675bd3408300cdb213c8
onsato <nogiglee@gmail.com>
2026-01-10 00:47:02 +0900

    event handler

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,5 +1,34 @@
-export default function Home() {
-	return (
-		<h1>hello world!</h1>
-	)
+"use client";
+
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
 };
+
+const isLoggedIn = true;
+
+function MyButton() {
+  function handleClick() {
+    alert("You clicked me!");
+  }
+  return <button onClick={handleClick}>Click me</button>;
+}
+
+export default function Home() {
+  return (
+    <div>
+      <h1>welcomw to my app</h1>
+      {isLoggedIn && <MyButton />}
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </div>
+  );
+}
