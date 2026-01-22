1d5fbebd99fa50cb84196b7e5a6b5a22d9371124
onc-limb <onclimb.eng@gmail.com>
2026-01-23 00:50:48 +0900

    winner

--- next-app/components/board.tsx
+++ next-app/components/board.tsx
@@ -7,8 +7,10 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   const handleClick = (i: number) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-    if (squares[i]) return;
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
@@ -17,8 +19,16 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   };
+  const winner = calculateWinner(squares);
+  let status: string;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -36,4 +46,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares: number[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
