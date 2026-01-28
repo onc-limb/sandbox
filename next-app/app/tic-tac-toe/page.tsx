4afa4974c491db562f4f47f7be955265bf93a3cc
onc-limb <onclimb.eng@gmail.com>
2026-01-28 23:51:46 +0900

    loop

--- next-app/app/tic-tac-toe/page.tsx
+++ next-app/app/tic-tac-toe/page.tsx
@@ -45,24 +45,24 @@ function Board({
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
+
+
+  const columns = squares.map((square, index) => (
+    <Square key={index} value={square} onSquareClick={() => handleClick(index)} />
+  ));
+
+  const rows = [];
+  for (let i = 0; i < 3; i++) {
+    rows.push(
+      <div key={i} className="board-row">
+        {columns.slice(i * 3, i * 3 + 3)}
+      </div>
+    );
+  }
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {rows}
     </>
   );
 }
